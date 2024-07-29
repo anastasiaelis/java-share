7c87b9c99b8f12fb148edf4e70a2f74b094d107e
anastasiaelis <anastasia_elis@mail.ru>
2024-07-29 12:21:02 +0500

    maven

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.service.ItemServiceDtoImpl;
-
 import javax.validation.Valid;
 import java.util.List;
 
