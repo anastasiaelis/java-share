b93fbc2a6f921b02a272adadbf1670259d248129
anastasiaelis <anastasia_elis@mail.ru>
2024-07-30 22:18:23 +0500

    email6

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -20,6 +20,6 @@ public class User {
     private Long id;
     @Column(name = "name", nullable = false)
     private String name;
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 }
