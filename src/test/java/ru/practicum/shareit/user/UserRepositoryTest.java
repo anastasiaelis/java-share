7ab691665e6810a558f751adb84d7e2a1d9e9a21
anastasiaelis <anastasia_elis@mail.ru>
2024-07-31 22:53:35 +0500

    @tfctfctf

--- src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -1,12 +1,13 @@
 package ru.practicum.shareit.user;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import ru.practicum.shareit.user.repository.UserRepository;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 class UserRepositoryTest {
